8247f203ff75a377b97b8018d0be754f0d29b4d6
Rick Lin <HengYi_lin@HTC.COM>
2012-08-30 17:32:57 +0800

    Add func for deleting each line item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :cart
   belongs_to :cart
   belongs_to :product
+  
+  def total_price
+    product.price * quantity
+  end
 end
